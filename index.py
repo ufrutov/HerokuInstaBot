604d48b316a54a346da40493f31f0a8c8fe771c1
ufrutov <mailluifruti@gmail.com>
2018-06-17 18:55:36 +0200

    Commit base url configuration

---
+++ index.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python
+import os
+import sys
+import web
+
+urls = (
+'/', 'index'
+)
+
+class index:
+    def GET(self):
+        i = web.input(name=None)
+        return render.index(i.name)
+
+if __name__ == "__main__":
+    app = web.application(urls, globals())
+    app.run()
+
+render = web.template.render('templates/')
